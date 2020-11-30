44c0d850bfb39650e1e069c9e8320be90cc594fb
Yuri <Yuribtr@users.noreply.github.com>
2020-11-30 23:27:36 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+from datetime import datetime as dt
+
+
+def get_employees():
+    print('Getting employees on ' + str(dt.now().date()))
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+from datetime import datetime as dt
+
+
+def calculate_salary():
+    print('Calculating salary on ' + str(dt.now().date()))
